5507328978bf6a4c6dd6b0dcab7c8fe6fce0cc9b
afzytk <afzytk@gmail.com>
2026-03-01 19:17:53 +0530

    fix: fix component issues

--- frontend/src/pages/Favorites.jsx
+++ frontend/src/pages/Favorites.jsx
@@ -1,28 +1,29 @@
-import "../css/Favorites.css"
-import { useMovieContext } from "../context/MovieContext"
-import MovieCard from "../components/MovieCard"
+import "../css/Favorites.css";
+import { useMovieContext } from "../context/MovieContext";
+import MovieCard from "../components/MovieCard";
 
-function favorites() {
-    const {favorites} = useMovieContext();
+function Favorites() {
+  const { favorites } = useMovieContext();
 
-    if (favorites) {
-        return (
-            <div className="favorites">
-               <h2>Your Favorites</h2>
-            <div className="movies-grid">
-                {favorites.map((movie) => (
-                 <MovieCard movie={movie} key={movie.id} />
-                ))}
-               </div>
-            </div>
-            );
+  if (favorites.length > 0) {
+    return (
+      <div className="favorites">
+        <h2>Your Favorites</h2>
+        <div className="movies-grid">
+          {favorites.map((movie) => (
+            <MovieCard movie={movie} key={movie.id} />
+          ))}
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div className="favorites-empty">
+      <h2>No Favorite movies yet</h2>
+      <p>start adding movies to your favorites and they will appear here</p>
+    </div>
+  );
 }
 
-return ( <div className="favorites-empty">
-    <h2>No Favorite movies yet</h2>
-    <p>start adding movies to your favorites and they will appear here</p>
-</div>
-);
-}
-
-export default favorites
+export default Favorites;
